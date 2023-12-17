e3feda9633e295293123853ffb142089adf0e4be
MihaiTirnaveanu <mihaitirnaveanu@gmail.com>
2023-12-17 20:56:28 +0200

    connect to psql database

---
+++ src/main/java/com/management/buildhub/Buildhub.java
@@ -0,0 +1,13 @@
+package com.management.buildhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buildhub {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buildhub.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/buildhub/BuildhubTests.java
@@ -0,0 +1,13 @@
+package com.management.buildhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildhubTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
